9bd0b0a35cec0579c85c7a89637b09b4faf89860
AOI-1up <aoi_1up@icloud.com>
2024-01-16 07:05:39 +0900

    feat: ファイル出力機能の実装

--- nextjs/components/Header.tsx
+++ nextjs/components/Header.tsx
@@ -1,9 +1,31 @@
+"use client";
+import { useAtomValue } from "jotai";
+import { BsCaretRight } from "react-icons/bs";
+import { CanvasElement } from "./atom/CanvasElement";
+import { convertToTerraform } from "@/utils/convertToTerraform";
+import { saveAs } from "file-saver";
+
 export const Header = () => {
+  const canvasElementArray = useAtomValue(CanvasElement);
+
+  const handleClick = () => {
+    const code = convertToTerraform(canvasElementArray);
+    const blob = new Blob([code], { type: "text/plain;charset=utf-8" });
+    saveAs(blob, "output.tf");
+  };
+
   return (
-    <div className="flex h-[50px] w-screen select-none items-center justify-center border-b-2 border-gray-400 bg-gray-800">
+    <div className="flex h-[50px] w-screen select-none items-center justify-between border-b-2 border-gray-400 bg-gray-800">
+      <div className="w-[70px]"></div>
       <div className="font-mono text-xl font-bold tracking-widest text-white">
         Polaris
       </div>
+      <button
+        className="flex h-full w-[70px] items-center justify-center text-white"
+        onClick={handleClick}
+      >
+        <BsCaretRight size="25px" />
+      </button>
     </div>
   );
 };
